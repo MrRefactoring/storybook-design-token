6eb9055e37f74631896b27a2b1b263c489a68970
Vladislav Tupikin <mrrefactoring@yandex.ru>
2025-06-12 14:20:19 +0300

    v4.0.1

--- addon/src/preview.ts
+++ addon/src/preview.ts
@@ -1,11 +1,7 @@
 import { ProjectAnnotations, Renderer } from "storybook/internal/types";
-import { PARAM_KEY } from "./constants";
 
 const preview: ProjectAnnotations<Renderer> = {
   decorators: [],
-  // globals: {
-  //   [PARAM_KEY]: false,
-  // },
 };
 
 export default preview;
